f291aa60d50a860af720e1977120d5ee7b03fd44
fdg <filippodegennaro@hotmail.it>
2026-05-10 16:48:59 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -7,6 +7,15 @@ export const metadata = {
 
 const CHANGELOG = [
   {
+    version: '1.10.0',
+    date: '10 maggio 2026',
+    label: 'Auto',
+    changes: [
+      'unify app version across home and changelog',
+    ],
+  },
+
+  {
     version: '1.9.2',
     date: '10 maggio 2026',
     label: 'Auto',
