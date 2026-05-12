1115b1c74f8a2da0ad0fb1807863b38a439d6f45
fdg <filippodegennaro@hotmail.it>
2026-05-12 20:52:06 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -8,6 +8,15 @@ export const metadata = {
 
 const CHANGELOG = [
   {
+    version: '1.13.20',
+    date: '12 maggio 2026',
+    label: 'Auto',
+    changes: [
+      'Auth mascot for password reset and improve avatar mapping',
+    ],
+  },
+
+  {
     version: '1.13.19',
     date: '12 maggio 2026',
     label: 'Auto',
