8a1506d36e699c13f5e38016bb1df079e3b37dbf
fdg <filippodegennaro@hotmail.it>
2026-05-13 15:48:21 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -29,6 +29,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.18.4',
+    date: '13 maggio 2026',
+    label: 'Auto',
+    changes: [
+      'install CTA login con bottom sheet e harden PWA head/manifest',
+    ],
+  },
+
+  {
     version: '1.18.3',
     date: '13 maggio 2026',
     label: 'Auto',
