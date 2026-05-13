849cd861d4bd12d29300db462bdee0dbe1532ef2
fdg <filippodegennaro@hotmail.it>
2026-05-13 14:00:54 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -29,6 +29,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.17.4',
+    date: '13 maggio 2026',
+    label: 'Auto',
+    changes: [
+      'renderizza tutti i link splash iOS nel head del RootLayout',
+    ],
+  },
+
+  {
     version: '1.17.3',
     date: '13 maggio 2026',
     label: 'Auto',
