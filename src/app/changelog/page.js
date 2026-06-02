d397aa7ccd31cd21bcb33e316a344fa60c65fb8b
fdg <filippodegennaro@hotmail.it>
2026-06-02 17:41:31 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -29,6 +29,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.29.0',
+    date: '2 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'harden import pipeline and add legacy schema fallbacks',
+    ],
+  },
+
+  {
     version: '1.28.5',
     date: '1 giugno 2026',
     label: 'Auto',
