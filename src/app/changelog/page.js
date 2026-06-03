772871c93fc7ddac2c9e606607faa1779bc569bd
fdg <filippodegennaro@hotmail.it>
2026-06-03 08:06:37 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -29,6 +29,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.29.3',
+    date: '3 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'documentation',
+    ],
+  },
+
+  {
     version: '1.29.2',
     date: '3 giugno 2026',
     label: 'Auto',
