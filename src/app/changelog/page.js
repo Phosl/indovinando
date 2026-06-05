9769b5ed8154048370cb29962de99dbe04939271
fdg <filippodegennaro@hotmail.it>
2026-06-05 08:58:59 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -29,6 +29,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.30.6',
+    date: '5 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'fix websearch token',
+    ],
+  },
+
+  {
     version: '1.30.5',
     date: '5 giugno 2026',
     label: 'Auto',
