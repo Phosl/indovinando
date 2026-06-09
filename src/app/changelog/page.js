a4adf67ce2d7eb705c6d5afd94884885e9805c77
fdg <filippodegennaro@hotmail.it>
2026-06-09 13:12:46 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -37,6 +37,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.43.4',
+    date: '9 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'Fix guided bottle repair save flow',
+    ],
+  },
+
+  {
     version: '1.43.3',
     date: '9 giugno 2026',
     label: 'Auto',
