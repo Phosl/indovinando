8ff0b3bd475f96fde44d5ee8292c6dc7124e3965
fdg <filippodegennaro@hotmail.it>
2026-06-09 11:06:56 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -37,6 +37,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.43.1',
+    date: '9 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'Refine delayed-answer table live flow',
+    ],
+  },
+
+  {
     version: '1.43.0',
     date: '9 giugno 2026',
     label: 'Auto',
