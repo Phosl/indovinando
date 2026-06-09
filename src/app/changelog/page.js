6d65faf32203bd7feaa79f6b4f894c5874bd63bb
fdg <filippodegennaro@hotmail.it>
2026-06-09 09:39:56 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -37,6 +37,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.42.0',
+    date: '9 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'refine game editor drag-and-drop controls and list actions',
+    ],
+  },
+
+  {
     version: '1.41.0',
     date: '9 giugno 2026',
     label: 'Auto',
