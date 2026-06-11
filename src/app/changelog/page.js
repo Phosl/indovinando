dd111ada1193826d7e940995d7f9b5ccbafd6557
fdg <filippodegennaro@hotmail.it>
2026-06-11 20:14:11 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -37,6 +37,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.44.6',
+    date: '11 giugno 2026',
+    label: 'Auto',
+    changes: [
+      'Refactor automatic tasting creation flow into modular components',
+    ],
+  },
+
+  {
     version: '1.44.5',
     date: '11 giugno 2026',
     label: 'Auto',
