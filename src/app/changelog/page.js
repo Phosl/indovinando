76357256fff1afdf348fb66026cf833f19fe9073
fdg <filippodegennaro@hotmail.it>
2026-07-27 17:36:34 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -39,6 +39,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.51.1',
+    date: '27 luglio 2026',
+    label: 'Auto',
+    changes: [
+      'align tasting detail actions',
+    ],
+  },
+
+  {
     version: '1.51.0',
     date: '27 luglio 2026',
     label: 'Auto',
