de046f60a35fbeee868cf08dd4fbd739554870c9
fdg <filippodegennaro@hotmail.it>
2026-07-30 11:43:59 +0200

    chore: update changelog

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -39,6 +39,15 @@ const UI_TEXT = {
 
 const CHANGELOG = [
   {
+    version: '1.52.0',
+    date: '30 luglio 2026',
+    label: 'Auto',
+    changes: [
+      'polish onboarding and persist dismissals',
+    ],
+  },
+
+  {
     version: '1.51.2',
     date: '27 luglio 2026',
     label: 'Auto',
